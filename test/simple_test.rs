ac800d1d6370fe8cefadb1f73ba9d8ccd2cef112
greg <elorian.mestec@gmail.com>
2015-10-11 01:36:15 -0700

    Add unicode to test file

--- test/simple_test.rs
+++ test/simple_test.rs
@@ -3,7 +3,10 @@ use std::io::Write;
 
 fn main() {
     println!("This goes to stdout");
+    println!("ユニコードを作りましょう");
     write!(&mut io::stderr(),
             "This goes to stderr\n").unwrap();
+    write!(&mut io::stderr(),
+            "ユニコードを作りましょう\n").unwrap();
 }
 
