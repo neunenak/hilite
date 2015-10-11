782faf778ad6e395189899ddbb41f8fef4f6191d
greg <elorian.mestec@gmail.com>
2015-10-11 00:46:52 -0700

    Add process-handling code

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 use std::env;
 use std::io;
 use std::io::Write;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,9 +17,11 @@ fn main() {
         return;
     }
 
-    let run_command = args.get(1).unwrap();
-    let (_, run_arguments) = args.split_at(2);
+    let program_name = args.get(1).unwrap();
+    let (_, program_args) = args.split_at(2);
 
-    println!("Running {} with args {:?}", run_command, run_arguments);
+    println!("Running {} with args {:?}", program_name, program_args);
 
+    let mut program_command: process::Command = process::Command::new(program_name);
+    let output = program_command.spawn().unwrap_or_else({|_| panic!("Failed to spawn program") });
 }
